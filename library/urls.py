803a08d3b53da8982b4caa50c7c7ef3847720162
raisulru <raisulru10@gmail.com>
2019-06-25 23:08:14 +0600

    Changed project name

--- library/urls.py
+++ library/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+admin.site.site_header = 'Library Management'
 urlpatterns = [
     path('', admin.site.urls),
     # path('api/v1/', include('users.urls')),
