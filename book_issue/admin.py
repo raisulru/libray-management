de657ee78e331e5b02839b4f42bf8d049c24badb
raisulru <raisulru10@gmail.com>
2019-06-27 00:39:36 +0600

    Changed message

--- book_issue/admin.py
+++ book_issue/admin.py
@@ -12,7 +12,7 @@ class IssueBookAdmin(admin.ModelAdmin):
 
     def send_mail(self, email=None, name='There', book='No Name', date=datetime.datetime.now()):
         subject = 'Hi {}'.format(name)
-        message = 'We inform you that you took a book name "{}" at {}. Your return date have been expired. Please contact with us as soon as possible. Thanks.'.format(book, date)
+        message = 'We informed you that you took a book "{}" at {}. Your return date has been expired. Please contact with us as soon as possible. Thanks.'.format(book, date)
         from_email = EMAIL_HOST_USER
         mails = send_mail(subject, message, from_email, [email])
 
@@ -23,7 +23,7 @@ class IssueBookAdmin(admin.ModelAdmin):
         queryset_withdraw = queryset.filter(status=WITHDRAW)
         email_list = []
         today = datetime.datetime.now().date()
-        
+
         for issued_book in queryset_withdraw:
             if today > issued_book.suggest_return_date:
                 self.send_mail(issued_book.user.email, issued_book.user.username, issued_book.book.name, issued_book.suggest_return_date)        
